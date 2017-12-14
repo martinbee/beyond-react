588b83dad1bc49729d4105d0321ba9a7c8ea5305
Martin Bee <martbee88@gmail.com>
2017-12-14 13:37:01 -0600

    feat: begin work on select

--- src/components/EditWorkout/FormElements/LiftType/index.jsx
+++ src/components/EditWorkout/FormElements/LiftType/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const LiftType = () => {
-  return <div>LiftType</div>;
-};
+function LiftType() {
+  return (
+    <select onChange={(evt) => console.log(evt.target.value)}>
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+      <option value="mercedes">Mercedes</option>
+      <option value="audi">Audi</option>
+    </select>
+  );
+}
 
 export default LiftType;
