3c09ab6c9c548a1cf617895c35a316bd077bfefa
Martin Bee <martbee88@gmail.com>
2017-12-16 09:44:56 -0600

    feat: refactor header component with popover

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 
-const Header = () => (
-  <div>
-    <AppBar />
-    <Popover />
-  </div>
-);
+const popoverDefaultProps = {
+  anchorOrigin: {
+    horizontal: 'left',
+    vertical: 'bottom',
+  },
+  targetOrigin: {
+    horizontal: 'left',
+    vertical: 'top',
+  },
+};
 
-export default Header;
+export default class Header extends Component {
+  state = {
+    isPopoverOpen: false,
+    popoverAnchorEl: null,
+  }
+
+  openPopoverMenu = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      isPopoverOpen: true,
+      popoverAnchorEl: event.currentTarget,
+    });
+  };
+
+  closePopoverMenu = () => this.setState({ isPopoverOpen: false });
+
+  renderAppBar() {
+    return (
+      <AppBar
+        title="Beyond 5/3/1"
+        onLeftIconButtonClick={this.openPopoverMenu}
+      />
+    );
+  }
+
+  renderPopover() {
+    const {
+      isPopoverOpen,
+      popoverAnchorEl,
+    } = this.state;
+
+    const popoverProps = {
+      ...popoverDefaultProps,
+      anchorEl: popoverAnchorEl,
+      open: isPopoverOpen,
+      onRequestClose: this.closePopoverMenu,
+    };
+
+    return (
+      <Popover {...popoverProps}>
+        {this.renderMenu()}
+      </Popover>
+    );
+  }
+
+  renderMenu() {
+    return (
+      <Menu>
+        <MenuItem primaryText="Refresh" />
+        <MenuItem primaryText="Help &amp; feedback" />
+        <MenuItem primaryText="Settings" />
+        <MenuItem primaryText="Sign out" />
+      </Menu>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderAppBar()}
+        {this.renderPopover()}
+      </div>
+    );
+  }
+}
+
