8f8141f0eb6a3ec1145aa5ebe503d2edbf2ff2d7
Martin Bee <martbee88@gmail.com>
2018-01-03 19:39:11 -0500

    feat: implement skeleton exercise Type edit

---
+++ src/components/EditWorkout/FormElements/Exercises/EditExercise/FormElements/Type/index.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  string,
+  func,
+} from 'prop-types';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+const options = [
+  {
+    primaryText: 'Warm up',
+    value: 'warmup',
+  },
+  {
+    primaryText: 'Core Lift',
+    value: 'coreLift',
+  },
+  {
+    primaryText: 'Joker Sets',
+    value: 'jokerSets',
+  },
+  {
+    primaryText: 'First Set Last',
+    value: 'firstSetLast',
+  },
+  {
+    primaryText: 'Accessory',
+    value: 'accessory',
+  },
+  {
+    primaryText: 'Cardio',
+    value: 'cardio',
+  },
+];
+
+function Type({ currentType, updateExercise }) {
+  const updateType = (evt, index, value) => updateExercise('Type', value);
+
+  const renderOptions = () => options.map(option => (
+    <MenuItem key={option.value} {...option} />
+  ));
+
+  return (
+    <SelectField
+      id="type"
+      floatingLabelText="Type"
+      value={currentType}
+      onChange={updateType}
+    >
+      {renderOptions()}
+    </SelectField>
+  );
+}
+
+export default Type;
--- src/components/EditWorkout/FormElements/Exercises/EditExercise/index.jsx
+++ src/components/EditWorkout/FormElements/Exercises/EditExercise/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import {
+  Type,
+} from './FormElements';
 
 // EditExercise needs to be composed of a dropdown for type, text field for
 // subtype, text area for notes, EditSets much like EditWorkout that handles
 // reps and weight and more can be added
 function EditExercise({ exercise, updateExercise, removeExercise }) {
-  const { _id, type, subType, sets, notes } = exercise;
+  const { type, subType, sets, notes } = exercise;
 
+  // handle exercises like workouts
   return (
     <div>
       <button onClick={removeExercise}>X</button>
-      {type}
+      <Type currentType={type} updateExercise={updateExercise} />
     </div>
   );
 }
